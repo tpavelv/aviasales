1a3afbf866c8bd773c6bbf245bf854c485c98d83
tpavelv <tpavelv@icloud.com>
2025-05-16 20:23:15 +0800

    Добавлен алерт на кнопку more

--- src/components/Button-More/ButtonMore.js
+++ src/components/Button-More/ButtonMore.js
@@ -3,7 +3,7 @@ import React from 'react'
 import styles from './ButtonMore.module.scss'
 
 const ButtonMore = () => (
-  <button type="button" className={styles['button-more']}>
+  <button type="button" className={styles['button-more']} onClick={() => alert('Пока не работает')}>
     Показать еще 5 билетов
   </button>
 )
